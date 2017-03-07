e516343855378209b79c8dfed917f90bfee8145b
arksnorman <arksnorman@gmail.com>
2017-03-06 19:51:12 -0500

    Removed procedual connect.php and created new class [classes/database.php] to connect to database

---
+++ classes/database.php
@@ -0,0 +1,19 @@
+<?php 
+	/**
+	* @author Kidhoma Norman
+	* Create new class to handle database duties
+	*/
+	class Database
+	{
+		//initialize static and private property which will store DB connection
+		private static $connect
+
+		//construct method to automatically connect to database when class is instantiated
+		function __construct()
+		{
+			//connect to database
+			self::$connect = mysqli_connect('127.0.0.1', 'root', 'root', 'test');
+			return self::$connect;
+		}
+	}
+?>
