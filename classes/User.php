cb1552dd1dc15f77020676c7cf2ac4abb302858b
arksnorman <arksnorman@gmail.com>
2017-03-09 18:24:09 -0500

    [classes/User.php] added new methods [isLoggedIn and logout

--- classes/User.php
+++ classes/User.php
@@ -15,7 +15,22 @@
 		public function login($username, $password)
 		{			
 			$query = $this->_db->query("SELECT * FROM test WHERE username = ? AND password = ?", array($username, $password));
-			return ($query->count() > 0) ? true : false;
+			return ($query->count() > 0) ? $_SESSION['user_session'] = $query->results()[0]->username : false;
+		}
+
+		public static function isLoggedIn()
+		{
+			if(isset($_SESSION['user_session']))
+			{
+				return true;
+			}
+		}
+
+		public static function logout()
+		{
+			session_destroy();
+			unset($_SESSION['user_session']);
+			return true;
 		}
 		
 	}
