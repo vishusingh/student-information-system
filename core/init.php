b772f7b3219139d0864d18cb2eb3ee2ee4e3c3ec
arksnorman <arksnorman@gmail.com>
2017-03-19 10:44:13 -0400

    [core/init.php] Added ROOT_PATH

--- core/init.php
+++ core/init.php
@@ -1,8 +1,10 @@
 <?php
 
+	define('ROOT_PATH', $_SERVER['DOCUMENT_ROOT']);	
+
 	function my_autoloader($class) 
 	{
-    	require_once $_SERVER['DOCUMENT_ROOT'] . '/classes/' . $class . '.php';
+    	require_once ROOT_PATH . '/classes/' . $class . '.php';
 	}
 
 	spl_autoload_register('my_autoloader');
