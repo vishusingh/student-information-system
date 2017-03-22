9fa25d50f6c1810332cfa5e8a5cfd034b4bc8e0e
arksnorman <arksnorman@gmail.com>
2017-03-22 18:50:00 -0400

    [core/init.php] Added error reporting

--- core/init.php
+++ core/init.php
@@ -1,5 +1,18 @@
 <?php
 
+	/**
+	* Report simple running errors
+	*/
+	error_reporting(E_ALL);
+
+	/**
+	* Display errors on screen
+	*/
+	ini_set('display_errors', 1);
+
+	/**
+	* Define root path i.e /var/www/student-information-system
+	*/
 	define('ROOT_PATH', $_SERVER['DOCUMENT_ROOT']);	
 
 	function my_autoloader($class) 
