37f3bd0a376dc1dc3541a1f187a1fe1395d81343
arksnorman <arksnorman@gmail.com>
2017-04-29 01:17:30 +0300

    [includes/footer.php] Added comments

--- includes/footer.php
+++ includes/footer.php
@@ -1,2 +1,13 @@
+</div>
+<!--START OVERALL ROW-->
+
+</div>
+<!--END OVERALL CONTAINER-->
+
+</body>
+
 <script type="text/javascript" src="/assets/js/jquery.js"></script>
+
 <script type="text/javascript" src="/assets/js/bootstrap.js"></script>
+
+</html>
