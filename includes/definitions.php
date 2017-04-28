a10c2b44a92b22ecd7154415e11ecf828bcbe4d9
arksnorman <arksnorman@gmail.com>
2017-04-29 00:41:47 +0300

    [includes/definitions.php] Defined side nav of system

--- includes/definitions.php
+++ includes/definitions.php
@@ -4,10 +4,13 @@
 	* @author Kidhoma Norman
 	*/
 
-	//Define footer of site
+	// Define footer of site
 	define('footer', rootPath . '/includes/footer.php');
 
-	//Define header of site
+	// Define header of site
 	define('header', rootPath . '/includes/header.php');
+
+	// Define Side NavBar 
+	define('sideNav', rootPath . '/includes/sideNav.php');	
 
 ?>
