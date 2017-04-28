994d55a3983362f2defd158cd1999b9765de4ba5
arksnorman <arksnorman@gmail.com>
2017-04-29 00:11:01 +0300

    [includes/side-nav.php] Applied class active function

--- includes/side-nav.php
+++ includes/side-nav.php
@@ -1,7 +1,7 @@
 <ul class="nav nav-pills nav-stacked">
-    <li <?php if ($section === 'home') { echo "class='active'"; } ?> ><a href="/home"><i class="fa fa-home"></i> Home</a></li>
-    <li <?php if ($section === 'timetables') { echo "class='active'"; } ?> ><a href="/content/timetables"><i class="fa fa-table"></i> Timetables</a></li>
-    <li <?php if ($section === 'classmates') { echo "class='active'"; } ?> ><a href="/content/classmates"><i class="fa fa-group"></i> Classmates</a></li>
-    <li <?php if ($section === 'results') { echo "class='active'"; } ?> ><a href="/content/results"><i class="fa fa-search"></i> Results</a></li>
-    <li <?php if ($section === 'download') { echo "class='active'"; } ?> ><a href="/content/download"><i class="fa fa-download"></i> Download Notes</a></li>
+    <li <?php classActive($section, 'services'); ?> ><a href="/home"><i class="fa fa-home"></i> Home</a></li>
+    <li <?php classActive($section, 'timetables'); ?> ><a href="/content/timetables"><i class="fa fa-table"></i> Timetables</a></li>
+    <li <?php classActive($section, 'classmates'); ?> ><a href="/content/classmates"><i class="fa fa-group"></i> Classmates</a></li>
+    <li <?php classActive($section, 'results'); ?> ><a href="/content/results"><i class="fa fa-search"></i> Results</a></li>
+    <li <?php classActive($section, 'download'); ?> ><a href="/content/download"><i class="fa fa-download"></i> Download Notes</a></li>
 </ul>
