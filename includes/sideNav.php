8f56b9f5a2fb2c72b411b50cda509e9827dad383
arksnorman <arksnorman@gmail.com>
2017-04-29 00:45:29 +0300

    [includes/sideNav.php] Renamed side-nav to sideNav

---
+++ includes/sideNav.php
@@ -0,0 +1,7 @@
+<ul class="nav nav-pills nav-stacked">
+    <li <?php classActive($section, 'services'); ?> ><a href="/home"><i class="fa fa-home"></i> Home</a></li>
+    <li <?php classActive($section, 'timetables'); ?> ><a href="/content/timetables"><i class="fa fa-table"></i> Timetables</a></li>
+    <li <?php classActive($section, 'classmates'); ?> ><a href="/content/classmates"><i class="fa fa-group"></i> Classmates</a></li>
+    <li <?php classActive($section, 'results'); ?> ><a href="/content/results"><i class="fa fa-search"></i> Results</a></li>
+    <li <?php classActive($section, 'download'); ?> ><a href="/content/download"><i class="fa fa-download"></i> Download Notes</a></li>
+</ul>
