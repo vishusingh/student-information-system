4a260dcab0197c574eba7dca15d559e1afa9fdec
arksnorman <arksnorman@gmail.com>
2017-04-29 00:34:33 +0300

    [includes/definitions.php] Defined header and footer of the system

--- includes/definitions.php
+++ includes/definitions.php
@@ -1,1 +1,13 @@
-definitions.php
+<?php
+
+	/**
+	* @author Kidhoma Norman
+	*/
+
+	//Define footer of site
+	define('footer', rootPath . '/includes/footer.php');
+
+	//Define header of site
+	define('header', rootPath . '/includes/header.php');
+
+?>
