1b79839bf14e62488bc632692268334c5d3470dc
arksnorman <arksnorman@gmail.com>
2017-04-29 00:48:52 +0300

    [includes/header.php] applied sideNav definition

--- includes/header.php
+++ includes/header.php
@@ -5,10 +5,15 @@
     <head>
 
         <meta http-equiv="content-type" content="text/html; charset=UTF-8"> 
+
         <meta charset="utf-8">
+
         <title>SIS | STUDENT</title>
+
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
         <link rel="stylesheet" type="text/css" href="/assets/css/bootstrap.css">
+
         <link rel="stylesheet" type="text/css" href="/assets/css/font-awesome.min.css">
 
     </head>
@@ -24,6 +29,7 @@
 			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
 			<span class="icon-toggle"></span>
 			</button>
+
 			<a class="navbar-brand" href="/admin">Student | Dashboard</a>
 
 		</div>
@@ -33,10 +39,13 @@
 			<ul class="nav navbar-nav navbar-right">
 
 			<li class="dropdown">
+
 				<a class="dropdown-toggle" role="button" data-toggle="dropdown"><i class="fa fa-user"></i>USER<span class="caret"></span></a>
+
 				<ul id="g-account-menu" class="dropdown-menu" role="menu">
 				<li><a href="/content/profile">Profile</a></li>
 				</ul>
+				
 			</li>
 
 			<li><a href="/logout.php"><i class="fa fa-sign-out fa-lg"></i> Logout</a></li>
@@ -49,3 +58,13 @@
 	<!-- /container -->
 
 </div>
+
+<div class="container">
+
+	<div class="row">
+
+		<div class="col-md-2">
+
+		<?php require_once sideNav; ?>
+
+		</div>
