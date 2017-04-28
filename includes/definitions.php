16f08cb95ff02348d67b524df41714bd5a57507b
arksnorman <arksnorman@gmail.com>
2017-04-29 01:31:35 +0300

    [includes/definitions.php] Updated comments

--- includes/definitions.php
+++ includes/definitions.php
@@ -4,10 +4,10 @@
 	* @author Kidhoma Norman
 	*/
 
-	// Define footer of site
+	// Define footer of system
 	define('footer', rootPath . '/includes/footer.php');
 
-	// Define header of site
+	// Define header of system
 	define('header', rootPath . '/includes/header.php');
 
 	// Define Side NavBar 
@@ -19,7 +19,7 @@
 	*  Define Views of system
 	*/
 
-	//Define home page of site
+	//Define home view of system
 	define('homeView', rootPath . '/views/home.php');
 
 	/**
