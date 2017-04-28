55359831b83d33ce45e9afe09176a659994b584f
arksnorman <arksnorman@gmail.com>
2017-04-29 00:21:55 +0300

    [includes/header.php] Rewrite header file

--- includes/header.php
+++ includes/header.php
@@ -1,22 +1,51 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+    <head>
+
+        <meta http-equiv="content-type" content="text/html; charset=UTF-8"> 
+        <meta charset="utf-8">
+        <title>SIS | STUDENT</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+        <link rel="stylesheet" type="text/css" href="/assets/css/bootstrap.css">
+        <link rel="stylesheet" type="text/css" href="/assets/css/font-awesome.min.css">
+
+    </head>
+    
+<body>
+
 <div id="top-nav" class="navbar navbar-default navbar-static-top">
-          <div class="container">
-            <div class="navbar-header">
-              <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                  <span class="icon-toggle"></span>
-              </button>
-              <a class="navbar-brand" href="/admin">Student | Dashboard</a>
-            </div>
-            <div class="navbar-collapse collapse">
-              <ul class="nav navbar-nav navbar-right">
-                
-                <li class="dropdown">
-                  <a class="dropdown-toggle" role="button" data-toggle="dropdown"><i class="fa fa-user"></i> <?php echo $_SESSION['user_session']; ?> <span class="caret"></span></a>
-                  <ul id="g-account-menu" class="dropdown-menu" role="menu">
-                    <li><a href="/content/profile">Profile</a></li>
-                  </ul>
-                </li>
-                <li><a href="/logout.php"><i class="fa fa-sign-out fa-lg"></i> Logout</a></li>
-              </ul>
-            </div>
-          </div><!-- /container -->
-        </div>
+
+	<div class="container-fluid">
+
+		<div class="navbar-header">
+
+			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+			<span class="icon-toggle"></span>
+			</button>
+			<a class="navbar-brand" href="/admin">Student | Dashboard</a>
+
+		</div>
+
+		<div class="navbar-collapse collapse">
+
+			<ul class="nav navbar-nav navbar-right">
+
+			<li class="dropdown">
+				<a class="dropdown-toggle" role="button" data-toggle="dropdown"><i class="fa fa-user"></i>USER<span class="caret"></span></a>
+				<ul id="g-account-menu" class="dropdown-menu" role="menu">
+				<li><a href="/content/profile">Profile</a></li>
+				</ul>
+			</li>
+
+			<li><a href="/logout.php"><i class="fa fa-sign-out fa-lg"></i> Logout</a></li>
+			
+			</ul>
+
+		</div>
+
+	</div>
+	<!-- /container -->
+
+</div>
