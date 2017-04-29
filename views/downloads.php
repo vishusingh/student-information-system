758d07552c76ac60d4e309a9f51befb9dcc57231
arksnorman <arksnorman@gmail.com>
2017-04-29 15:40:50 +0300

    [views/downloads.php] Added downloads view

---
+++ views/downloads.php
@@ -0,0 +1,53 @@
+<!--START COLUMN 10-->
+<div class="col-md-10">
+
+	<!--START ROW-->
+	<div class="row">
+
+		<div class="col-md-12">
+
+			<div class="table-responsive"> 
+
+				<table class="table table-hover table-bordered">
+
+					<thead>
+
+						<tr>
+
+							<th>No.</th>
+							<th>Lecturer</th>
+							<th>Course unit</th>
+							<th>Year</th>
+							<th>Name</th>
+							<th>Download</th>
+							
+						</tr>
+
+					</thead>
+
+					<tbody>
+
+						<tr>
+
+							<td>1</td>
+							<td>Anna</td>
+							<td>Operating Sys</td>
+							<td>Year 1 Sem 2</td>
+							<td>Intrduction to O.S</td>
+							<td><span class="fa fa-download"></span></td>
+
+						</tr>
+
+					</tbody>
+
+				</table>
+
+			</div>
+
+		</div><!--/col-span-12-->
+
+	</div>
+	<!--END ROW-->
+
+</div>
+<!--END COLUMN 10-->
