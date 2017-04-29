70f737efd5d4cdee96df60dbf6a60bea12d1cec7
arksnorman <arksnorman@gmail.com>
2017-04-29 21:11:24 +0300

    [includes/functions.php] Added function to pic results from database

--- includes/functions.php
+++ includes/functions.php
@@ -1,11 +1,30 @@
 <?php
 
-	// function to print class active for the navbar
+	// function to print class active for the navbars
 	function classActive($page, $listItem)
 	{
 
 		return $page == $listItem ? print 'class="active"' : print '';
 				
+	}
+
+	// Custom function to return results from database
+	function results(string $queryString)
+	{
+
+		$results = array();
+
+		$query = Database::getInstance()->query($queryString);
+
+		if ($query->count() > 0)
+		{
+			
+			$results = $query->results();
+
+		}
+
+		return $results;
+
 	}
 
 ?>
