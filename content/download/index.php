cde6a5cad583c084bae59758f4eb13b4e9dd749c
arksnorman <arksnorman@gmail.com>
2017-04-29 15:41:39 +0300

    [content/download/index.php] Rewrite download index page

--- content/download/index.php
+++ content/download/index.php
@@ -1,71 +1,13 @@
 <?php
-    require_once($_SERVER['DOCUMENT_ROOT'] . '/core/init.php');
-    $section = 'download';
-    if(!User::isLoggedIn())       
-    {   
-        Redirect::to('/login');        
-    }           
+
+	$section = 'download';
+
+	require_once '../../core/init.php';
+
+	require_once header;
+
+	require_once downloadsView;
+
+	require_once footer;
+
 ?>
-<!DOCTYPE html>
-<html lang="en-US">
-    <head>
-        <meta http-equiv="content-type" content="text/html; charset=UTF-8"> 
-        <meta charset="utf-8">
-        <title>SIS | STUDENT</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-        <link href="/assets/css/bootstrap.css" rel="stylesheet" type="text/css">
-        <link href="/assets/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-    </head>
-    
-    <body>
-        
-        <!-- Header -->
-            <?php require_once(ROOT_PATH . '/includes/header.php'); ?>
-        <!-- /Header -->
-
-        <!-- Main -->
-        <div class="container">
-        <div class="row">
-            <!-- start col-2 -->
-            <div class="col-md-2">
-                <?php require_once(ROOT_PATH . '/includes/side-nav.php'); ?>
-            </div>
-            <!-- end col-2 -->
-
-            <div class="col-md-10">
-           
-                <div class="row">  
-                    
-                    <div class="col-md-12">
-                      <div class="table-responsive">          
-                      <table class="table table-hover table-bordered">
-                        <thead>
-                          <tr>
-                            <th>No.</th>
-                            <th>Lecturer</th>
-                            <th>Course unit</th>
-                            <th>Year</th>
-                            <th>Name</th>
-                            <th>Download</th>
-                          </tr>
-                        </thead>
-                        <tbody>
-                          <tr>
-                            <td>1</td>
-                            <td>Anna</td>
-                            <td>Operating Sys</td>
-                            <td>Year 1 Sem 2</td>
-                            <td>Intrduction to O.S</td>
-                            <td><span class="fa fa-download"></span></td>
-                          </tr>
-                        </tbody>
-                      </table>
-                      </div>
-                    </div><!--/col-span-12-->             
-              </div><!--/row-->
-            </div><!--/col-span-9-->
-        </div>
-        </div>
-    <?php require_once(ROOT_PATH . '/includes/footer-scripts.php'); ?>
-    </body>
-</html>
