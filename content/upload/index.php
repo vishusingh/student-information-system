a593f11aec0f8b1f3a1a1eb2e58fab5706f65967
arksnorman <arksnorman@gmail.com>
2017-04-30 09:26:03 +0300

    [content/upload/index.php] Added php code for file mgt

--- content/upload/index.php
+++ content/upload/index.php
@@ -2,7 +2,14 @@
 
 	$section = 'upload';
 
-	require_once '../../core/init.php';  
+	require_once '../../core/init.php'; 
+
+	if (isset($_POST['submit'])) 
+	{
+
+		print_r($_FILES['file']);
+		
+	} 
 
 	require_once header;
 
