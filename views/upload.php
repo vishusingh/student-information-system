f74de72019d67ed21fd82514cf4f27fd4c9c738b
arksnorman <arksnorman@gmail.com>
2017-04-30 09:26:41 +0300

    [views/upload.php] Added form for upload file

--- views/upload.php
+++ views/upload.php
@@ -4,7 +4,37 @@
 	<!--START ROW-->
 	<div class="row">
 
-	
+		<div class="col-md-4">
+
+			<form action="" method="POST" enctype="multipart/form-data" role="form">
+
+				<legend>Upload Notes</legend>				
+				
+				<input type="file" class="filestyle" name="file" data-buttonBefore="true">
+
+				<hr>
+
+				<button type="submit" name="submit" class="btn btn-info">Upload</button>
+
+			</form>
+
+		</div>
+
+		<div class="col-md-4">
+
+			<form action="" method="POST" enctype="multipart/form-data" role="form">
+
+				<legend>Upload Timetable</legend>				
+				
+				<input type="file" class="filestyle" name="file" data-buttonBefore="true">
+
+				<hr>
+
+				<button type="submit" name="submit" class="btn btn-info">Upload</button>
+
+			</form>
+
+		</div>
 
 	</div>
 	<!--END ROW-->
