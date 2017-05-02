7248487d9c53a5ab1847e91ff8a5bdab562af6db
arksnorman <arksnorman@gmail.com>
2017-05-02 19:41:19 +0300

    [includes/functions.php] Refactor uploaderrors array

--- includes/functions.php
+++ includes/functions.php
@@ -29,16 +29,17 @@
 
 	function uploadErrors() : array
 	{
+
 		return	array(
-				UPLOAD_ERR_OK => 'Your file was uploaded successfuly',
-				UPLOAD_ERR_INI_SIZE => 'The uploaded file exceeds the upload_max_filesize directive in php.ini.',
-				UPLOAD_ERR_FORM_SIZE => 'The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form',
-				UPLOAD_ERR_PARTIAL => 'The uploaded file was only partially uploaded.',
-				UPLOAD_ERR_NO_FILE => 'No file was uploaded.',
-				UPLOAD_ERR_NO_TMP_DIR => 'Missing a temporary folder. Introduced in PHP 5.0.3.',
-				UPLOAD_ERR_CANT_WRITE => 'Failed to write file to disk. Introduced in PHP 5.1.0.',
-				UPLOAD_ERR_EXTENSION => 'upload error extension'
-				);
+		UPLOAD_ERR_OK 			=> 'Your file was uploaded successfuly',
+		UPLOAD_ERR_INI_SIZE 	=> 'The uploaded file exceeds the upload max filesize.',
+		UPLOAD_ERR_FORM_SIZE 	=> 'The uploaded file exceeds the form upload max filesize.',
+		UPLOAD_ERR_PARTIAL 		=> 'The uploaded file was only partially uploaded. Try re-uploading the file again please.',
+		UPLOAD_ERR_NO_FILE 		=> 'No file was uploaded.',
+		UPLOAD_ERR_NO_TMP_DIR 	=> 'Upload Internal Server Error. Try again later.',
+		UPLOAD_ERR_CANT_WRITE 	=> 'Upload Internal Server Error. Try again later.',
+		UPLOAD_ERR_EXTENSION 	=> 'Upload Internal Server Error. Try again later.'
+		);
 
 	}
 
