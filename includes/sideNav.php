20617ee2143c0fd53ee3a6fda626adf671c4af70
arksnorman <arksnorman@gmail.com>
2017-05-02 19:48:04 +0300

    [includes/sideNav.php] change download notes to notes

--- includes/sideNav.php
+++ includes/sideNav.php
@@ -19,16 +19,16 @@
     <li <?php classActive($section, 'students'); ?> ><a href="<?php echo $baseUrl; ?>/content/students/"><i class="fa fa-group"></i> Students</a>
     </li>
 
-    <li <?php classActive($section, 'timeTables'); ?> ><a href="<?php echo $baseUrl; ?>/content/timetables/"><i class="fa fa-table"></i> Timetables</a>
-    </li>
-
     <li <?php classActive($section, 'results'); ?> ><a href="<?php echo $baseUrl; ?>/content/results/"><i class="fa fa-search"></i> Results</a>
     </li>
 
     <li <?php classActive($section, 'upload'); ?> ><a href="<?php echo $baseUrl; ?>/content/upload/"><i class="fa fa-upload"></i> Upload File</a>
     </li>
 
-    <li <?php classActive($section, 'download'); ?> ><a href="<?php echo $baseUrl; ?>/content/download/"><i class="fa fa-download"></i> Download Notes</a>
+    <li <?php classActive($section, 'timeTables'); ?> ><a href="<?php echo $baseUrl; ?>/content/timetables/"><i class="fa fa-table"></i> Timetables</a>
+    </li>
+
+    <li <?php classActive($section, 'download'); ?> ><a href="<?php echo $baseUrl; ?>/content/download/"><i class="fa fa-book"></i> Notes</a>
     </li>
 
 </ul>
@@ -47,16 +47,16 @@
     <li <?php classActive($section, 'home'); ?> ><a href="<?php echo $baseUrl; ?>/home"><i class="fa fa-home"></i> Home</a>
     </li>
 
-    <li <?php classActive($section, 'timeTables'); ?> ><a href="<?php echo $baseUrl; ?>/content/timetables/"><i class="fa fa-table"></i> Timetables</a>
-    </li>
-
     <li <?php classActive($section, 'classMates'); ?> ><a href="<?php echo $baseUrl; ?>/content/classmates/"><i class="fa fa-group"></i> Classmates</a>
     </li>
 
     <li <?php classActive($section, 'results'); ?> ><a href="<?php echo $baseUrl; ?>/content/results/"><i class="fa fa-search"></i> Results</a>
     </li>
 
-    <li <?php classActive($section, 'download'); ?> ><a href="<?php echo $baseUrl; ?>/content/download/"><i class="fa fa-download"></i> Download Notes</a>
+    <li <?php classActive($section, 'timeTables'); ?> ><a href="<?php echo $baseUrl; ?>/content/timetables/"><i class="fa fa-table"></i> Timetables</a>
+    </li>
+
+    <li <?php classActive($section, 'download'); ?> ><a href="<?php echo $baseUrl; ?>/content/download/"><i class="fa fa-book"></i> Notes</a>
     </li>
     
 </ul>
