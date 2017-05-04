4ad457fde2b806b5e165152b55fb953bbeb7a039
arksnorman <arksnorman@gmail.com>
2017-05-04 20:25:54 +0300

    [views/adminHome.php] Refactor admin home page

--- views/adminHome.php
+++ views/adminHome.php
@@ -7,63 +7,6 @@
 		<!--START COLUMN 6 ONE-->
 		<div class="col-md-6">
 
-			<div class="panel panel-default">
-
-				<div class="panel-heading"><h4>Course Units</h4></div>
-
-				<div class="panel-body">
-
-					<small>Operating Systems</small>
-
-					<div class="progress">
-						<div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="72" aria-valuemin="0" aria-valuemax="100" style="width: 72%">
-						<span class="sr-only">72% Complete</span>
-						</div>
-					</div>
-
-					<small>C programming</small>
-
-					<div class="progress">
-						<div class="progress-bar progress-bar-info" role="progressbar" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100" style="width: 20%">
-						<span class="sr-only">20% Complete</span>
-						</div>
-					</div>
-
-					<small>Web Design</small>
-
-					<div class="progress">
-						<div class="progress-bar progress-bar-warning" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%">
-						<span class="sr-only">60% Complete (warning)</span>
-						</div>
-					</div>
-
-					<small>Repair and Maintanence</small>
-
-					<div class="progress">
-						<div class="progress-bar progress-bar-danger" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width: 80%">
-						<span class="sr-only">80% Complete</span>
-						</div>
-					</div>
-
-				</div>
-			</div>
-
-			<div class="panel panel-default">
-
-				<div class="panel-heading"><h4>New Requests</h4></div>
-
-				<div class="panel-body">
-
-					<div class="list-group">
-					<a href="#" class="list-group-item active">Hosting virtual mailbox serv..</a>
-					<a href="#" class="list-group-item">Dedicated server doesn't..</a>
-					<a href="#" class="list-group-item">RHEL 6 install on new..</a>
-					</div>
-
-				</div>
-
-			</div>
-
 		</div>
 		<!--END COLUMN 6 ONE-->
 
@@ -72,78 +15,52 @@
 
 			<div class="panel panel-default">
 
-				<div class="panel-heading"><h4>Announcements</h4></div>
+				<div class="panel-heading"><h5>Announcements</h5></div>
 
 				<div class="panel-body">
-					<div style="display: none;" class="alert alert-info in">
-						<button type="button" class="close" data-dismiss="alert">×</button>
-						This is a dismissable alert.. just sayin'.
-						</div>
-
-					This is a dashboard-style layout that uses Bootstrap 
-					3. You can use this template as a starting point to create something 
-					more unique.
-					<br><br>
-					Visit the Bootstrap Playground at <a href="http://usebootstrap.com/theme/admin">Bootply</a> to tweak this layout or discover more useful code snippets.
+					<p>Content from database here</p>
 				</div>
 
 			</div>
 
-			<table class="table table-striped">
-				<thead>
-				<tr><th colspan="3">Top Students</th></tr>
-				</thead>
-				<tbody>
-				<tr><td>45</td><td>2.45%</td><td>Math</td></tr>
-				</tbody>
-			</table>
+			<form action="<?php echo $baseUrl; ?>/home/" method="POST" role="form">
 
-			<div class="panel panel-default">
+				<?php
 
-				<div class="panel-heading">
-					<div class="panel-title">
-					<i class="fa fa-wrench pull-right"></i>
-					<h4>Post Suggestion Or Complaint</h4>
-					</div>
-				</div>
+					if (isset($errorList)) 
+					{
+						
+						foreach ($errorList as $error) 
+						{
+							
+							echo '<div class="alert alert-danger">' . $error . '</div>';
 
-				<div class="panel-body">
+						}
 
-					<form class="form form-vertical">
+					}
 
-						<div class="control-group">
-							<label>Message</label>
-							<div class="controls">
-							<textarea class="form-control"></textarea>
-							</div>
-						</div> 
+					if (isset($_GET['status']) && $_GET['status'] == 'complaitsuccess') 
+					{
+						
+						echo '<div class="alert alert-success"><p>Thanks for your feedback</p></div>';
 
-						<div class="control-group">
-							<label>Category</label>
-							<div class="controls">
-								<select class="form-control">
-									<option></option>
-									<option>Lecturer</option>
-									<option>Suggestion</option>
-									<option>Other</option>
-								</select>
-							</div>
-						</div>    
+					}
 
-						<div class="control-group">
-							<label></label>
-							<div class="controls">
-								<button type="submit" class="btn btn-primary">
-								send
-								</button>
-							</div>
-						</div>   
+				?>
 
-					</form>
+				<legend><h3>Add Announcement</h3></legend>
+			
+				<div class="form-group">
 
-				</div>
+					<label for="message">Message</label>
 
-			</div>
+					<textarea class="form-control" name="message" rows="3" id="message"></textarea>
+
+				</div>					
+			
+				<button type="submit" name="submit" class="btn btn-info">Submit</button>
+
+			</form>
 
 		</div>
 		<!--END COLUMN 6 TWO-->
