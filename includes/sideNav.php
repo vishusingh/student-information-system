131a5212d5ca3ee19242b6a10d9d9282f2082733
arksnorman <arksnorman@gmail.com>
2017-05-05 23:18:09 +0300

    [includes/sideNav.php] correct paths for notes

--- includes/sideNav.php
+++ includes/sideNav.php
@@ -28,7 +28,7 @@
     <li <?php classActive($section, 'timeTables'); ?> ><a href="<?php echo $baseUrl; ?>/content/timetables/"><i class="fa fa-table"></i> Timetables</a>
     </li>
 
-    <li <?php classActive($section, 'download'); ?> ><a href="<?php echo $baseUrl; ?>/content/download/"><i class="fa fa-book"></i> Notes</a>
+    <li <?php classActive($section, 'download'); ?> ><a href="<?php echo $baseUrl; ?>/content/notes/"><i class="fa fa-book"></i> Notes</a>
     </li>
 
 </ul>
