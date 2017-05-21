a52e5e8b59071ff8e80d3f0c3136bfb3b266c5bd
arksnorman <arksnorman@gmail.com>
2017-05-22 01:00:55 +0300

    [views/addstudent.php] Added some fields for students

--- views/addstudent.php
+++ views/addstudent.php
@@ -4,7 +4,83 @@
 	<!--START ROW-->
 	<div class="row">
 
-	
+		<h1 class="text-center">Add Student</h1>
+
+		<hr />
+
+		<div class="col-md-6">
+
+			<form action="/content/addstudent/" method="POST" role="form">
+			
+				<div class="form-group">
+					<label for="fname">First Name</label>
+					<input type="text" class="form-control" id="fname" placeholder="Enter First Name">
+				</div>
+
+				<div class="form-group">
+					<label for="lname">Last Name</label>
+					<input type="text" class="form-control" id="lname" placeholder="Enter Last Name">
+				</div>
+
+				<div class="form-group">
+					<label for="dob">D.O.B</label>
+					<input type="text" class="form-control" id="dob" placeholder="Enter First Name">
+				</div>
+
+				<div class="form-group">
+					<label for="lname">Last Name</label>
+					<input type="text" class="form-control" id="lname" placeholder="Enter Last Name">
+				</div>
+
+				<div class="form-group">
+					<label for="fname">First Name</label>
+					<input type="text" class="form-control" id="fname" placeholder="Enter First Name">
+				</div>
+
+				<div class="form-group">
+					<label for="lname">Last Name</label>
+					<input type="text" class="form-control" id="lname" placeholder="Enter Last Name">
+				</div>
+
+		</div>
+
+		<div class="col-md-6">
+			
+				<div class="form-group">
+					<label for="fname">First Name</label>
+					<input type="text" class="form-control" id="fname" placeholder="Enter First Name">
+				</div>
+
+				<div class="form-group">
+					<label for="lname">Last Name</label>
+					<input type="text" class="form-control" id="lname" placeholder="Enter Last Name">
+				</div>
+
+				<div class="form-group">
+					<label for="fname">First Name</label>
+					<input type="text" class="form-control" id="fname" placeholder="Enter First Name">
+				</div>
+
+				<div class="form-group">
+					<label for="lname">Last Name</label>
+					<input type="text" class="form-control" id="lname" placeholder="Enter Last Name">
+				</div>
+
+				<div class="form-group">
+					<label for="fname">First Name</label>
+					<input type="text" class="form-control" id="fname" placeholder="Enter First Name">
+				</div>
+
+				<div class="form-group">
+					<label for="lname">Last Name</label>
+					<input type="text" class="form-control" id="lname" placeholder="Enter Last Name">
+				</div>				
+			
+				<button type="submit" submit="submit" class="btn btn-info">Add Student</button>
+
+			</form>
+
+		</div>
 
 	</div>
 	<!--END ROW-->
