d09e5f0f1df9ccbcbce9cec58e1f8e23479e5b44
arksnorman <arksnorman@gmail.com>
2017-09-04 02:11:53 +0300

    [views/login.php] Added login

---
+++ views/login.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Login</title>
+        <link href="/css/bootstrap.min.css" rel="stylesheet" />
+        <link href="/css/bootstrap-theme.css" rel="stylesheet" />
+        <link href="/css/elegant-icons-style.css" rel="stylesheet" />
+        <link href="/css/font-awesome.css" rel="stylesheet" />
+        <link href="/css/style.css" rel="stylesheet" />
+        <link href="/css/style-responsive.css" rel="stylesheet" />
+    </head>
+    <body class="login-img3-body">
+        <div class="container">
+        <form class="login-form" action="index.html">
+            <div class="login-wrap">
+                <p class="login-img"><i class="icon_lock_alt"></i></p>
+                <div class="input-group">
+                    <span class="input-group-addon"><i class="icon_profile"></i></span>
+                    <input type="text" class="form-control" placeholder="Username" autofocus>
+                </div>
+                <div class="input-group">
+                    <span class="input-group-addon"><i class="icon_key_alt"></i></span>
+                    <input type="password" class="form-control" placeholder="Password">
+                </div>
+                <label class="checkbox">
+                <input type="checkbox" value="remember-me"> Remember me
+                <!-- <span class="pull-right"> <a href="#"> Forgot Password?</a></span> -->
+                </label>
+                <button class="btn btn-primary btn-lg btn-block" type="submit">Login</button>
+                <!-- <button class="btn btn-info btn-lg btn-block" type="submit">Signup</button> -->
+            </div>
+        </form>
+    </body>
+</html>
