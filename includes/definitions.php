2db381a4819fa54f7ede723427356af3a741cf65
arksnorman <arksnorman@gmail.com>
2017-09-15 02:58:07 +0300

    added definitions

---
+++ includes/definitions.php
@@ -0,0 +1,8 @@
+<?php
+
+	define('DBHOST', Config::get('database/host'));
+	define('DBUSERNAME', Config::get('database/username'));
+	define('DBPASS', Config::get('database/password'));
+	define('DBNAME', Config::get('database/dbname'));
+	define('DBPORT', Config::get('database/port'));
+	define('DBDRIVER', Config::get('database/driver'));
