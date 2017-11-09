3b11d0ccb399cc48ccc17a5558336badb67947c0
Kidhoma Norman <arksnorman@gmail.com>
2017-11-10 02:38:03 +0300

    UI Change

---
+++ src/common/footer.php
@@ -0,0 +1,55 @@
+<footer class="footer text-right">
+<?=FOOTNOTE;?>
+</footer>
+</div>
+<!-- ============================================================== -->
+<!-- End Right content here -->
+<!-- ============================================================== -->
+
+</div>
+<!-- END wrapper -->
+
+<script>
+var resizefunc = [];
+</script>
+
+<!-- Plugins  -->
+<script src="<?=JSPATH;?>jquery.min.js"></script>
+<script src="<?=JSPATH;?>popper.min.js"></script><!-- Popper for Bootstrap -->
+<script src="<?=JSPATH;?>bootstrap.min.js"></script>
+<script src="<?=JSPATH;?>detect.js"></script>
+<script src="<?=JSPATH;?>fastclick.js"></script>
+<script src="<?=JSPATH;?>jquery.slimscroll.js"></script>
+<script src="<?=JSPATH;?>jquery.blockUI.js"></script>
+<script src="<?=JSPATH;?>waves.js"></script>
+<script src="<?=JSPATH;?>wow.min.js"></script>
+<script src="<?=JSPATH;?>jquery.nicescroll.js"></script>
+<script src="<?=JSPATH;?>jquery.scrollTo.min.js"></script>
+<script src="http://coderthemes.com/minton/plugins/switchery/switchery.min.js"></script>
+
+<!-- Counter Up  -->
+<script src="http://coderthemes.com/minton/plugins/waypoints/lib/jquery.waypoints.min.js"></script>
+<script src="http://coderthemes.com/minton/plugins/counterup/jquery.counterup.min.js"></script>
+
+<!--Morris Chart-->
+<script src="http://coderthemes.com/minton/plugins/morris/morris.min.js"></script>
+<script src="http://coderthemes.com/minton/plugins/raphael/raphael-min.js"></script>
+
+<!-- Page js  -->
+<script src="<?=JSPATH;?>jquery.dashboard.js"></script>
+
+<!-- Custom main Js -->
+<script src="<?=JSPATH;?>jquery.core.js"></script>
+<script src="<?=JSPATH;?>jquery.app.js"></script>
+
+<script type="text/javascript">
+jQuery(document).ready(function($) {
+$('.counter').counterUp({
+delay: 100,
+time: 1200
+});
+});
+</script>
+
+</body>
+</html>
---
+++ web/index.php
@@ -0,0 +1,61 @@
+<?php
+
+session_start();
+
+define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'development');
+define('DS', DIRECTORY_SEPARATOR);
+define('ROOTPATH', __DIR__ . DS . '..');
+define('VIEWPATH', ROOTPATH . DS . 'src' . DS . 'templates' . DS);
+define('INCPATH', ROOTPATH . DS . 'src' . DS . 'common' . DS);
+define('HEADER', INCPATH . 'header.php');
+define('FOOTER', INCPATH . 'footer.php');
+define('CONTROLLERPATH', ROOTPATH . DS . 'src' . DS . 'pages' . DS);
+define('CLASSPATH', ROOTPATH . DS . 'app' . DS . 'classes' . DS);
+define('CSSPATH', '/static/css/');
+define('JSPATH', '/static/js/');
+define('FONTSPATH', '/static/fonts/');
+define('IMAGEPATH', '/static/images/');
+define('FOOTNOTE', date('Y') . ' © Student Information System.');
+
+switch (ENVIRONMENT)
+{
+	case 'development':
+		error_reporting(-1);
+		ini_set('display_errors', 1);
+	break;
+
+	case 'testing':
+	case 'production':
+		ini_set('display_errors', 0);
+		if (version_compare(PHP_VERSION, '7.0', '>='))
+		{
+			error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED & ~E_STRICT & ~E_USER_NOTICE & ~E_USER_DEPRECATED);
+		}
+		else
+		{
+			error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT & ~E_USER_NOTICE);
+		}
+	break;
+
+	default:
+		header('HTTP/1.1 503 Service Unavailable.', TRUE, 503);
+		echo 'The application environment is not set correctly.';
+		exit(1);
+}
+
+function autoloader($className)
+{
+	if (file_exists(CLASSPATH.$className.'.php'))
+	{
+		require_once CLASSPATH.$className.'.php';
+	}
+	else
+	{
+		require_once CONTROLLERPATH.$className.'.php';
+	}
+}
+
+spl_autoload_register('autoloader');
+require_once INCPATH . 'definitions.php';
+require_once INCPATH . 'functions.php';
+FrontController::runRequest();
