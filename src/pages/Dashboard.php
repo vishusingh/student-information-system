89f8e900faf00a06b3ba334eed20557ac74c104e
Kidhoma Norman <arksnorman@gmail.com>
2017-11-13 14:44:06 +0300

    Add complaints on dashboard

--- src/pages/Dashboard.php
+++ src/pages/Dashboard.php
@@ -9,7 +9,7 @@
 			$admins = Database::getInstance()->query('SELECT * FROM admins')->count();
 			$lecturers = Database::getInstance()->query('SELECT * FROM lecturers')->count();
 			$notices = Database::getInstance()->query('select * from notices order by notice_id desc limit 2')->results();
-			$complaints = Database::getInstance()->query('select * from complaints order by complaints.complaint_id desc limit 5;')->results();
+			$complaints = Database::getInstance()->query('select * from complaints order by complaints.complaint_id desc limit 3')->results();
 			require_once HEADER;
 			require_once $this->view('dashboard');
 			require_once FOOTER;
--- src/templates/dashboard.php
+++ src/templates/dashboard.php
@@ -76,51 +76,30 @@
         </div>
 
         <div class="row">
-            <div class="col-md-4">
-                <div class="card m-b-20">
-                    <div class="card-header">
-                        Complaint
+			<?php
+			if (isset($complaints) && count($complaints))
+			{
+				foreach ($complaints as $complaint)
+				{
+					?>
+                    <div class="col-md-4">
+                        <div class="card m-b-20">
+                            <div class="card-header">
+                                <?=$complaint->complaint_title;?>
+                            </div>
+                            <div class="card-body">
+                                <blockquote class="card-bodyquote">
+                                    <p><?=$complaint->complaint_message;?></p>
+                                    <div class="card-footer">Added by <cite title="Source Title"><?=$complaint->complaint_creator;?> | <?=$complaint->complaint_date;?></cite>
+                                    </div>
+                                </blockquote>
+                            </div>
+                        </div>
                     </div>
-                    <div class="card-body">
-                        <blockquote class="card-bodyquote">
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere
-                                erat a ante.</p>
-                            <footer>Someone famous in <cite title="Source Title">Source Title</cite>
-                            </footer>
-                        </blockquote>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-4">
-                <div class="card m-b-20">
-                    <div class="card-header">
-                        Complaint
-                    </div>
-                    <div class="card-body">
-                        <blockquote class="card-bodyquote">
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere
-                                erat a ante.</p>
-                            <footer>Someone famous in <cite title="Source Title">Source Title</cite>
-                            </footer>
-                        </blockquote>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-4">
-                <div class="card m-b-20">
-                    <div class="card-header">
-                        Complaint
-                    </div>
-                    <div class="card-body">
-                        <blockquote class="card-bodyquote">
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere
-                                erat a ante.</p>
-                            <footer>Someone famous in <cite title="Source Title">Source Title</cite>
-                            </footer>
-                        </blockquote>
-                    </div>
-                </div>
-            </div>
+					<?php
+				}
+			}
+			?>
         </div>
         <!-- end row -->
 
